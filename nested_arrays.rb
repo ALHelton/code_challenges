cb5fb5a2d7c5cf3a2d8a5cd2c0ecdde933f10f2e
Andra Helton <andrahelton@gmail.com>
2023-06-12 23:24:17 -0600

    Add first solution

---
+++ nested_arrays.rb
@@ -0,0 +1,21 @@
+# Ruby built in method/function to flatten arrays - .flatten
+# recreate this functionality without using .flatten
+# remove the deeply nested brackets to return a single array
+
+# input: a deeply nested array, or multi-dimensional array
+# output: a one-dimensional array
+
+def bracket_crusher(input)
+  removed_brackets = input.join(" ")
+  new_array = removed_brackets.split(" ")
+  new_array.map do |element|
+    if element.to_i.to_s == element
+      element.to_i
+    else
+      element
+    end
+  end
+end
+
+p bracket_crusher([1, 2, 3, [[4], 5], [[[6]]]])
+# p bracket_crusher(["hi", "this is", [[["string"], "that is very"], [[[["nested"]]]]]])
