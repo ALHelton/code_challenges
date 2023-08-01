7163eb91b513fead4861efb062ad24c5302ae1bd
Andra Helton <andrahelton@gmail.com>
2023-07-31 19:29:11 -0600

    fix: corrected roman_numerals hash

--- roman_numerals.rb
+++ roman_numerals.rb
@@ -6,22 +6,24 @@
 # takes actual number value and place value and finds appropriate roman numberal character
 # puts it all into one string
 
-"M": 1000
-"CM": 900
-"D": 500
-"CD": 400
-"C": 100
-"XC": 90
-"L": 50
-"XL": 40
-"X": 10
-"IX": 9
-"V": 5
-"IV": 4
-"I": 1
+roman_numerals = {
+  "M" => 1000,
+  "CM" => 900,
+  "D" => 500,
+  "CD" => 400,
+  "C" => 100,
+  "XC" => 90,
+  "L" => 50,
+  "XL" => 40,
+  "X" => 10,
+  "IX" => 9,
+  "V" => 5,
+  "IV" => 4,
+  "I" => 1
+}
 
 def to_roman(number)
-
+  require 'pry'; binding.pry
 end
 
 p to_roman(128)
