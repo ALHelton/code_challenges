6594309fd07119ec540f1a88e9cca9645216cb3e
Andra Helton <andrahelton@gmail.com>
2023-08-08 17:52:48 -0600

    Add solution 2 for reverse_integer

--- reverse_integer.rb
+++ reverse_integer.rb
@@ -15,29 +15,40 @@
 # if false, will return 0
 
 def reverse(x)
-  num_array = x.to_s.reverse.chars
-  if num_array.last == "-"
-    rev_num = num_array.insert(0, num_array.pop).join.to_i
-  else
-    rev_num = num_array.join.to_i
-  end
+  rev_num = x.to_s.reverse.to_i
+  rev_num *= -1 if x < 0
+  range = (-2.pow(31)..2.pow(31) - 1)
 
-  range_result = in_range?(rev_num)
-  if range_result == true
-    rev_num
-  else
-    0
-  end
-end
-
-def in_range?(num)
-  high = 2.pow(31) - 1
-  low = -2.pow(31)
-  range = (low..high)
-  range.include?(num)
+  return 0 unless range.include?(rev_num)
+  rev_num
 end
 
 # p reverse(123)
 # p reverse(120)
 # p reverse(-123)
 p reverse(-1230)
+
+# ------------- FIRST SOLUTION --------------
+# def reverse(x)
+#   num_array = x.to_s.reverse.chars
+#   if num_array.last == "-"
+#     rev_num = num_array.insert(0, num_array.pop).join.to_i
+#   else
+#     rev_num = num_array.join.to_i
+#   end
+
+#   range_result = in_range?(rev_num)
+#   if range_result == true
+#     rev_num
+#   else
+#     0
+#   end
+# end
+
+# def in_range?(num)
+#   high = 2.pow(31) - 1
+#   low = -2.pow(31)
+#   range = (low..high)
+#   range.include?(num)
+# end
+# ------------------------------------------
