97889e1eed41c1b27de3fdf2af2c8ff424bdaf89
Andra Helton <andrahelton@gmail.com>
2023-08-09 20:44:01 -0600

    Add in-progress solution for asteroid_collision

---
+++ asteroid_collision.rb
@@ -0,0 +1,53 @@
+# input: array of integers (positive or negative)
+# output: array of remaining integers from input array
+
+# NOTES
+# direction of ints: positive = right, negative = left
+# if remaining ints are both positive, or both negative, they both stay
+# (going the same direction)
+# 
+# bigger size = bigger number
+# Examples:
+# -10 is as big as 10
+# 5 is smaller than -10
+# 5 is smaller than 10
+
+# PSEUDOCODE
+# method takes argument of an array of integers
+# iterate through the array and determine if positive or negative number
+# if int is positive, will be compared to the integer after it
+# if int is negative, will be compared to the integer before it
+
+def asteroid_collection(array)
+  new_array = []
+
+  array.each do |num|
+    if new_array.last.nil?
+      new_array << num
+      next
+    elsif (new_array.last > 0 && num < 0) || (new_array.last < 0 && num > 0)
+      if new_array.last.abs < num.abs
+        new_array.pop
+        new_array << num
+      elsif new_array.last.abs == num.abs
+        new_array.pop
+        next
+      else
+        next
+      end
+    else
+      new_array << num
+    end
+  end
+end
+
+# loop until all elements in array are the same(pos OR neg)? 
+
+# p asteroid_collection([5, 10, -5])
+# => [5,10]
+
+# p asteroid_collection([8,-8])
+# => []
+
+p asteroid_collection([10, 2, -5]) #NOT WORKING
+# => [10]
