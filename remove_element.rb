ec2d5661d70042d7dee4e246bd7896ba1f44cb95
Andra Helton <andrahelton@gmail.com>
2023-08-10 10:40:50 -0600

    Add solution one for remove_element

--- remove_element.rb
+++ remove_element.rb
@@ -10,11 +10,25 @@
 
 
 def remove_element(nums, val)
+  new_nums = []
+  underscores = []
   
+  nums.each do |num|
+    if num == val
+      num = "_"
+      underscores << num
+    else
+      new_nums << num
+    end
+  end
+
+  count = new_nums.size
+  nums = (new_nums << underscores).flatten
+  count
 end
 
-p remove_element([3,2,2,3], 3)
+# p remove_element([3,2,2,3], 3)
 # => 2 (nums = [2, 2, _, _])
 
 p remove_element([0,1,2,2,3,0,4,2], 2)
-# => 5 (nums = [0, 1, 3, 0, 4, 2])
+# => 5 (nums = [0,1,4,0,3,_,_,_]) # does not matter what order it's returned
