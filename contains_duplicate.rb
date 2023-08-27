5488a7f322c56e4f4dd298aeb713b16ecee4deec
Andra Helton <andrahelton@gmail.com>
2023-08-27 13:10:11 -0600

    Add pseudocode for contains_duplicate

---
+++ contains_duplicate.rb
@@ -0,0 +1,9 @@
+# input: array of integers
+# output: boolean
+
+# NOTES
+# need to take the array and count how many times each element appears in array
+# put into a hash with key being the number, value the count of times in nums array
+# need to iterate through the hash 
+# if any number's value count is > 1, return false
+# if number's value count < 1, return true
