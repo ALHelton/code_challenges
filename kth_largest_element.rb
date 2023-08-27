b600ad796ba222815cd2c45a03ef154b52209bce
Andra Helton <andrahelton@gmail.com>
2023-08-27 15:15:05 -0600

    Add pseudocode for kth_largest_element

---
+++ kth_largest_element.rb
@@ -0,0 +1,21 @@
+# input: 2 arguments
+# # 1: array of integers
+# # 2: integer
+
+# NOTES:
+# takes k integer
+# sorts the array, reverses
+# iterates through the array
+# finds the kth integer index in the array
+# returns the number in that place
+# (duplicates count as separate number, not bundled together)
+
+def find_kth_largest(nums, k)
+
+end
+
+p find_kth_largest([3,2,1,5,6,4], 2)
+# => 5
+
+p find_kth_largest([3,2,3,1,2,4,5,5,6], 4)
+# => 4
