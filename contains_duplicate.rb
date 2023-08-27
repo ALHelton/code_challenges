ee7a8c93f941631819ddcd41807908f5de043905
Andra Helton <andrahelton@gmail.com>
2023-08-27 13:22:43 -0600

    Add refactor for contains_duplicate

--- contains_duplicate.rb
+++ contains_duplicate.rb
@@ -8,17 +8,13 @@
 # if any number's value count is > 1, return false
 # if number's value count < 1, return true
 
-
 def contains_duplicate(nums)
   num_count = nums.tally
-  num_count.each do |num|
-    if num[1] > 1
-      return true
-    else
-      return false
-    end
+  num_count.any? do |num|
+    num[1] > 1
   end
 end
+
 
 p contains_duplicate([1,2,3,1])
 # => true
@@ -28,3 +24,17 @@ p contains_duplicate([1,2,3,4])
 
 p contains_duplicate([1,1,1,3,3,4,3,2,4,2])
 # => true
+
+
+# ------------- SOLUTION ONE ---------------
+
+# def contains_duplicate(nums)
+#   num_count = nums.tally
+#   num_count.each do |num|
+#     if num[1] > 1
+#       return true
+#     else
+#       return false
+#     end
+#   end
+# end
