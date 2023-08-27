d728f9eda543253eb4c34402a447e0235ef9fb97
Andra Helton <andrahelton@gmail.com>
2023-08-27 16:59:26 -0600

    Add solution one for longest_palindromic_substring - needs refactor

--- longest_palindromic_substring.rb
+++ longest_palindromic_substring.rb
@@ -17,3 +17,44 @@
 # if variable inside loop has more elements than string variable has characters,
 # make string variable equal new element array.join
 
+def longest_palindrome(s)
+  s_array = s.chars
+  longest_string = ""
+
+  if s_array.size > 2
+    s_array.each_with_index do |char, index|
+      new_array = []
+      right_index = index + 1
+      left_index = index - 1
+
+      until s_array[right_index] != s_array[left_index] || s_array[right_index].size > s.length
+        new_array << char
+        new_array << s_array[right_index] if right_index < s.length
+        new_array.unshift(s_array[left_index]) if left_index >= 0
+        right_index += 1
+        left_index -= 1
+      end
+      longest_string = new_array.join if longest_string.length < new_array.size
+    end
+    longest_string
+  elsif s_array.size == 2 && s_array[0] == s_array[1]
+    s
+  else
+    "No palindromic substrings"
+  end
+end
+
+p longest_palindrome("b")
+# => "No palindromic substrings"
+
+p longest_palindrome("ba")
+# => "No palindromic substrings"
+
+p longest_palindrome("bb")
+# => "bb"
+
+p longest_palindrome("babad")
+# => "bab" OR "aba"
+
+p longest_palindrome("cbbd") # NOT WORKING
+# => "bb"
