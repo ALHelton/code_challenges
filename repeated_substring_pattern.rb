4b1c7a53ab6064ba742546e3a90e82acbd77b018
Andra Helton <andrahelton@gmail.com>
2023-08-28 16:50:59 -0600

    Add pseudocode

---
+++ repeated_substring_pattern.rb
@@ -0,0 +1,17 @@
+# input: string
+# output: boolean
+
+# NOTES
+# input string is all lowercase english letters (no spaces)
+# input string cannot be empty, must be at least 1 char
+# substring can be used 2 or more times to complete the full string
+
+# PSEUDOCODE
+# get number of characters in the string, set to variable
+# need to take the string, turn into array
+# set new variable string to empty string
+# until loop?
+# character gets appended to new variable string until
+# variable string can be divided from original string evently
+# if evenly -> return true
+# else false
