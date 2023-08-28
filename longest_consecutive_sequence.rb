eaa64f225be3568dd124c556e8953c2309d9bd48
Andra Helton <andrahelton@gmail.com>
2023-08-28 16:25:02 -0600

    Delete notes line

--- longest_consecutive_sequence.rb
+++ longest_consecutive_sequence.rb
@@ -3,7 +3,6 @@
 
 # NOTES
 # input array is unsorted
-# only able to iterate once for 0(n) complexity --> Solve first, worry about later
 # there can be duplicate numbers - duplicates count as one in sequence -
 # ex: 0, 0, 1, 2 => 3
 # can be negative numbers
