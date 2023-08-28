d51eff8ad991b1bd712ef8689e9a1a48fc026149
Andra Helton <andrahelton@gmail.com>
2023-08-28 15:37:02 -0600

    Add pseudocode

---
+++ majority_element.rb
@@ -0,0 +1,18 @@
+# input: array of integers
+# output: integer
+
+# NOTES:
+# Will always be a number that appears most in array
+# need a way to count how many times each element appears in an array
+# could put into a hash with the number as a key, and value as the count
+# then need to find the max value, and return the key
+
+def majority_element(nums)
+
+end
+
+p majority_element([3,2,3])
+# => 3
+
+p majority_element([2,2,1,1,1,2,2])
+# => 2
