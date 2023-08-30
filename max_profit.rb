9f52aea10ad93776be3896894e6f0bf41dbe8516
Andra Helton <andrahelton@gmail.com>
2023-08-30 15:16:45 -0600

    Add pseudocode

---
+++ max_profit.rb
@@ -0,0 +1,21 @@
+# input: array of integers
+# output: one integer
+
+# NOTES
+# The input array must have at least one number, less than 105
+# the prices must be a positive number 
+# assumption that prices must be a whole number
+# Must find the difference between 2 numbers = profit
+# number can only be considered for subtraction if it is 
+# after the first number in the array order
+
+# PSEUDOCODE
+# need to iterate through the array of integers
+# need to iterate again through the remaining numbers and compare 
+# to the current element
+# need a counter variable
+# if the difference is more than 0, add to the counter variable
+# if the counter variable number is higher than the difference, next
+# return the counter variable number
+
+
